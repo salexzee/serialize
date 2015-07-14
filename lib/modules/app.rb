155fab0f68806c2b530f9a84719b31f71b76d955
salexzee <samwebb@codingtestsite.com>
2015-07-14 17:59:14 +0930

    Start working on removing code from serialize.rb

--- lib/modules/app.rb
+++ lib/modules/app.rb
@@ -3,4 +3,11 @@ module App
 	VERSION = "1.0"
 	COPYRIGHT = "2015"
 	CREATOR = "Sam Webb"
+
+	def display
+		puts "Name: #{NAME}"
+		puts "Version: #{VERSION}"
+		puts "Copyright: #{COPYRIGHT}"
+		puts "Creator: #{CREATOR}"
+	end
 end
