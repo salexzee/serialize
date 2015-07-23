05cd80cc39e91c64d41a80cd52667b20224b9db0
salexzee <samwebb@codingtestsite.com>
2015-07-23 19:37:58 +0930

    Updated tests

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe Container do
   describe '#add_item' do
     it 'adds an item to the @items array' do
       container = Container.new
-      container.add_item('Macbook Air', '1234567890')
-      expect(container.items[0].to_s).to eq("Macbook Air(1234567890)")
+      container.add_item('Macbook Air', '1234567890', 300)
+      expect(container.items[0].to_s).to eq("Macbook Air(1234567890) - $300")
     end
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/serialize/item'
 RSpec.describe Item do
   describe '#initialize' do
     # Set serialize variable
-    item = Item.new('Macbook Air', '1234567890')
+    item = Item.new('Macbook Air', '1234567890', 300)
 
     it 'initializes with name set to passed in value' do
       expect(item.name).to eq('Macbook Air')
@@ -11,6 +11,10 @@ RSpec.describe Item do
 
     it 'initializes with serial_number set to passed in value' do
       expect(item.serial_number).to eq('1234567890')
+    end
+
+    it 'initializes with a price' do
+      expect(item.price).to eq(300)
     end
   end
 end
